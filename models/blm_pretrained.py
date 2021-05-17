6b7bbc54fcbc379557e0b483b8cc6f91dbc4b623
liyucheng <liyucheng_2@qq.com>
2021-05-17 11:35:42 +0800

    pblm测试通过

--- models/blm_pretrained.py
+++ models/blm_pretrained.py
@@ -5,23 +5,57 @@ import torch.nn as nn
 from . torch_utils import get_canvas, sample_permutation, seq_cross_entropy, collect, batch_randint, select
 import torch.nn.functional as F
 import torch
+from lyc.utils import get_optimizer_and_schedule
+from .lm import LM
 
 
 class PBLM(BLM):
     def __init__(self, hparams):
-        super(PBLM, self).__init__(hparams)
-        self.hparams=hparams
+        super(LM, self).__init__()
+        self.hparams=vars(hparams)
 
-        self.enc=AutoModel.from_pretrained(hparams.plm_name)
+        self.word = nn.Linear(hparams.d_model, hparams.vocab_size, bias=False)
+        nn.init.xavier_normal_(self.word.weight)
+        self.x_logit_scale = 1.
+
+        self.enc=AutoModel.from_pretrained(hparams.model_name_or_path, cache_dir=hparams.cache_dir)
+        self.enc.src_word_emb = self.enc.embeddings.word_embeddings
 
         if hparams.share_emb_prj_weight:
             self.word.weight = self.enc.embeddings.word_embeddings.weight
             self.x_logit_scale = (hparams.d_model ** -0.5)
+        
+        self.loc = nn.Linear(hparams.d_model, 1)
+        self.lrb = nn.Sequential(
+            nn.Linear(hparams.d_model * 2, hparams.d_model * 2),
+            nn.ReLU(),
+            nn.Linear(hparams.d_model * 2, 4)
+        )
     
     def forward_encoder(self, canvas):
-        output=self.enc(canvas)
+        attention_mask = torch.ones_like(canvas)
+        attention_mask = attention_mask.where(canvas!=self.hparams.pad_token_id, torch.tensor(0))
+        output=self.enc(canvas, attention_mask=attention_mask)
         return output.last_hidden_state
     
+    def training_step(self, batch, batch_idx):
+        input_ids, n, n_real = batch['input_ids'], batch['n'], batch['n_real']
+        losses = self('losses', input_ids, n, n_real)
+        return {**losses, 'log': {**losses}}
+    
+    def losses(self, seq, n, n_real):
+        k = batch_randint(0, n - 1)
+        rank = sample_permutation(seq)
+        keep = (rank < k.unsqueeze(1))
+        canvas, blanks, rest, loc, lb, rb = get_canvas(seq, keep, n)
+        loss_loc, loss_word, loss_lrb = self.get_loss(seq, canvas, blanks, rest, loc, lb, rb)
+        nll_lb = (loss_loc + loss_word + loss_lrb) * n.float() - (n + 1).float().lgamma()
+        return {'loss': nll_lb.sum() / n_real.sum(),
+                'loc': loss_loc.mean(),
+                'word': loss_word.mean(),
+                'lrb': loss_lrb.mean()
+                }
+
     def get_loss(self, seq, canvas, blanks, rest, loc, lb, rb):
         count = (rest != -1).sum(1)
         output = self.forward_encoder(canvas)
@@ -35,13 +69,18 @@ class PBLM(BLM):
         output_loc = collect(output_blank, loc)
 
         logits_word = self.word(output_loc) * self.x_logit_scale
-        target = collect(seq, rest, 0)
-        loss_word = seq_cross_entropy(logits_word, target, 0)
+        target = collect(seq, rest, self.hparams.pad_token_id)
+        loss_word = seq_cross_entropy(logits_word, target, self.hparams.pad_token_id)
         loss_word = loss_word.sum(1) / count.float()
-        output_word = torch.cat((output_loc, self.enc.embeddings.word_embeddings(target)), -1)
+        output_word = torch.cat((output_loc, self.enc.src_word_emb(target)), -1)
 
         logits_lrb = self.lrb(output_word)
         loss_lrb = seq_cross_entropy(logits_lrb, lb * 2 + rb, -3)
         loss_lrb = loss_lrb.sum(1) / count.float()
 
         return loss_loc, loss_word, loss_lrb
+    
+    def configure_optimizers(self):
+        params_group=[{'params': [param for name, param in self.named_parameters() if 'enc' in name], 'lr': self.hparams.sm_lr},
+                {'params': [param for name, param in self.named_parameters() if 'enc' not in name], 'lr': self.hparams.lr}]
+        return get_optimizer_and_schedule(params_group, lr=self.hparams.lr)
---
+++ train_pblm.py
@@ -0,0 +1,56 @@
+from lyc.train import get_args, TrainingArgs
+from dataclasses import dataclass
+from lyc.utils import (get_tokenizer,
+                       get_pl_callbacks)
+from lyc.data import (get_tokenized_ds,
+                      processor,
+                      get_dataloader)
+from models.blm_pretrained import PBLM
+import pytorch_lightning as pl
+import torch
+
+@dataclass
+class PblmArgs(TrainingArgs):
+    is_zh : bool = True
+    ds_name : str = 'train'
+    d_model : int = 768
+
+if __name__ == '__main__':
+    args = get_args(PblmArgs)
+    args.gpus = torch.cuda.device_count()
+    args.multigpu = torch.cuda.device_count() > 1
+
+    tokenizer=get_tokenizer(args.tokenizer_name_or_path, 
+            is_zh=args.is_zh, max_length = args.max_sent_len, min_sent_length = args.min_sent_len)
+    ds = get_tokenized_ds(args.dataset_scripts, 
+        args.train_data, tokenizer, max_length = args.max_sent_len, 
+        min_sent_length=args.min_sent_len, shuffle=True, tokenize_func='wo_padding')
+    train_ds=ds[args.ds_name]
+
+    processor.block_size = 512
+    processor.tokenizer = tokenizer
+    train_ds = train_ds.map(
+        processor.lm_group_texts,
+        batched=True
+    )
+    train_ds = train_ds.map(
+        processor.get_true_length,
+        batched=True
+    )
+    train_dl=get_dataloader(train_ds, batch_size=24, 
+        cols=['input_ids', 'n', 'n_real'])
+
+    args.vocab_size = tokenizer.vocab_size
+    args.pad_token_id = tokenizer.pad_token_id
+    model = PBLM(args)
+    callbacks = get_pl_callbacks(args)
+
+    trainer = pl.Trainer(
+        max_epochs = args.epoches, 
+        gpus = args.gpus,
+        distributed_backend = 'ddp' if args.multigpu else None,
+        log_every_n_steps = args.log_steps,
+        default_root_dir = args.root_dir  
+    )
+
+    trainer.fit(model, train_dataloader=train_dl)
